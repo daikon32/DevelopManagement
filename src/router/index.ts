45f9eb1a46bf1b5b67b86c16157d8e60505fc8c7
diakon32 <hikujaga366@gmail.com>
2022-08-28 16:31:55 +0900

    feat:各コンポーネントのテンプレートを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: () => import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/ProgressManagement",
+    name: "progress-management",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "@/views/ProgressManagement.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "@/views/Login.vue"),
   },
 ];
 
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 style="text-align: center">Home画面です</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+});
+</script>
